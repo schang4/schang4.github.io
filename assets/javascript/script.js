28ec2bc65012e8b0b556e294d7b97bdca4f9b0da
schang4 <schang4@berkeley.edu>
2016-11-27 19:16:06 -0800

    hello hello

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,42 @@
 $(document).ready(function() {
 
-	$("#link").click(function() {
+	$("#gallery").click(function() {
 		$("#modal-container").show();
-	});
-
-	$("#cancel").click(function() {
-		$("#modal-container").hide();
-	});
-
-	$("#submit").click(function() {
-		$("#modal-container").hide();
 	});
 
 	$("#modal-overlay").click(function() {
 		$("#modal-container").hide();
 	});
 
+	$("#europe-image").hover(function() {
+		$("#europe").hide();
+	});
+
+	$("#peru-image").hover(function() {
+		$("#peru").hide();
+	});
+
+	$("#thailand-image").hover(function() {
+		$("#thailand").hide();
+	});
+
+	$("#europe-container").hover(function() {
+		$("#europe").show();
+	});
+
+	$("#peru-container").hover(function() {
+		$("#peru").show();
+	});
+
+	$("#thailand-container").hover(function() {
+		$("#thailand").show();
+	});
+
+	$('.grid').masonry({
+	  // options...
+	  itemSelector: '.grid-item',
+	  columnWidth: 200
+	});
 });
+
+// When you leave the hover, I want the name "europe" to show up again. And make the page not recenter because of hiding "europe"
